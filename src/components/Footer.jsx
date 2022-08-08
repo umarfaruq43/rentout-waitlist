edd07e9951c6de8439b6a19d4e0f4d6f85bc7969
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-08-08 21:28:37 +0100

    update: foter fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,26 +10,31 @@ import Logo from "../assets/img/AppLogo.svg";
 const Footer = () => {
   return (
     <div className="bg-footer md:mt-20 mt-20 py-4 md:py-10 md:px-32">
-      <div className="flex justify-between items-center text-center text-very_dim md:text-left md:flex-row flex-col md:text-2xl md:text-primary font-bold">
+      <div className="flex justify-between items-center text-center text-very_dim md:text-left md:flex-row flex-col md:text-xl md:text-primary font-bold">
         <div className="text-center md:text-left mt-5">
           <img src={Logo} alt="" className="w-40  md:w-40  mx-auto md:mx-0" />
-          <div className="mt-4 md:text-2xl text-very_dim md:text-primary">
-            Lekki Phase 2, 
-            Lagos Nigeria
+          <div className="mt-4 md:text-xl text-very_dim md:text-primary">
+            Lekki Phase 2, Lagos Nigeria
           </div>
         </div>
 
-        <div className=" md:hidden my-4 text-center text-lg md:text-2xl text-primary md:text-right ">
-          <a href="mailto:hello@rentout.app">hello@rentout.app</a>
+        <div className=" md:hidden my-4 text-center text-md md:text-xl text-primary md:text-right ">
+          <a href="mailto:hey@rentout.app">hey@rentout.app</a>
         </div>
 
         <div className="mx-auto md:mx-0  md:mt-5 ">
           <div className="text-2xl text-very_dim md:text-orange flex justify-center md:justify-end mb-4">
-            <a href="#" className="footer_link">
+            <a
+              href="https://instagram.com/rentout.app?igshid=YmMyMTA2M2Y="
+              className="footer_link"
+            >
               <FaInstagram />
             </a>
 
-            <a href="#" className="footer_link">
+            <a
+              href="https://twitter.com/RentOutng?t=uYUlrohQarKhLMiRVlD3HA&s=09"
+              className="footer_link"
+            >
               <FaTwitter />
             </a>
 
@@ -37,13 +42,16 @@ const Footer = () => {
               <FaFacebookF />
             </a>
 
-            <a href="#" className="footer_link">
+            <a
+              href="https://www.linkedin.com/company/rentoutapp/"
+              className="footer_link"
+            >
               <FaLinkedin />
             </a>
           </div>
 
-          <div className="hidden md:block text-center text-lg text-primary md:text-right ">
-            <a href="mailto:hello@rentout.app">hello@rentout.app</a>
+          <div className="hidden md:block text-center text-md text-primary md:text-right ">
+            <a href="mailto:hey@rentout.app">hey@rentout.app</a>
           </div>
 
           <div className=" md:text-right">
--- src/components/Poll.jsx
+++ src/components/Poll.jsx
@@ -71,30 +71,30 @@ const Poll = ({ handleChange, clientData }) => {
         <div
           className={`${
             !poll ? "hidden" : "block"
-          }  md:mt-10 flex items-end flex-col`}
+          } mt-5 md:mt-10 flex items-end flex-col`}
         >
           <div className="progress">
             <div className="progress_indicator ">
-              <div className="w-full bg-gray-200">
+              <div className="w-full bg-gray-200 overflow-hidden">
                 <div
                   className="bg-primary"
                   style={{ width: `${totalRenting ? totalRenting : 0}%` }}
                 ></div>
               </div>
             </div>
-            <div className="progress_title">Renting Items</div>
+            <div className="progress_title mb-3">Renting Items</div>
           </div>
 
           <div className="progress">
             <div className="progress_indicator ">
-              <div className="w-full bg-gray-200">
+              <div className="w-full bg-gray-200 overflow-hidden">
                 <div
                   className="bg-orange"
                   style={{ width: `${totalRentOut ? totalRentOut : 0}%` }}
                 ></div>
               </div>
             </div>
-            <div className="progress_title">Renting out my Items</div>
+            <div className="progress_title mb-3 mt-5">Renting out my Items</div>
           </div>
         </div>
       </div>
